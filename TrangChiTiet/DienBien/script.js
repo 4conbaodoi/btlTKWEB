d576bd5a013e7aef356fecaec948cec38d8a76ce
baobao6226 <nthuhuyen1437@gmail.com>
2023-12-29 13:25:30 +0700

    them tour lien quan

--- TrangChiTiet/DienBien/script.js
+++ TrangChiTiet/DienBien/script.js
@@ -114,5 +114,32 @@ function showSuccessMessage() {
 }
 
 
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+});
     
